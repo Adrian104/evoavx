cf337040de0784d8a0ec39d2807c86949176e9a1
Adrian Kulawik <a.kulawik8@gmail.com>
2025-02-02 15:50:21 +0100

    Add basic CMake project structure

---
+++ core/include/evoavx/evoavx-impl/global.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#if !defined(EVO_OS_WINDOWS) && (defined(_WIN32) || defined(_WIN64))
+#define EVO_OS_WINDOWS
+#endif
+
+#if !defined(EVO_OS_LINUX) && defined(__linux__)
+#define EVO_OS_LINUX
+#endif
+
+#if defined(EVO_OS_WINDOWS) && defined(EVO_OS_LINUX)
+#error "Invalid OS configuration"
+#endif
+
+#ifdef EVO_OS_WINDOWS
+#include <immintrin.h>
+#include <windows.h>
+#endif
+
+#ifdef EVO_OS_LINUX
+#include <immintrin.h>
+#include <unistd.h>
+#endif
+
+#include <cstdint>
+
+namespace evo
+{
+	using f32_t = float;
+	using f64_t = double;
+	using i8_t = std::int8_t;
+	using i16_t = std::int16_t;
+	using i32_t = std::int32_t;
+	using i64_t = std::int64_t;
+	using u8_t = std::uint8_t;
+	using u16_t = std::uint16_t;
+	using u32_t = std::uint32_t;
+	using u64_t = std::uint64_t;
+}
---
+++ core/include/evoavx/evoavx.hpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "evoavx-impl/global.hpp"
