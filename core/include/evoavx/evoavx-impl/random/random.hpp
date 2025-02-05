48e1f3e7fbd21baf6cb2d70609ef00b695b2d6a6
Adrian Kulawik <a.kulawik8@gmail.com>
2025-02-05 17:34:57 +0100

    Remove random integer generation

--- core/include/evoavx/evoavx-impl/random/random.hpp
+++ core/include/evoavx/evoavx-impl/random/random.hpp
@@ -30,8 +30,6 @@ namespace evo
 	{
 	public:
 		__m512d next_512d() noexcept;
-		__m512i range_512i_52(u64_t range) noexcept;
-		__m512i range_512i_52(__m512i range) noexcept;
 	};
 }
 
@@ -45,58 +43,5 @@ namespace evo
 		__m512d converted = _mm512_cvtepu64_pd(shifted);
 
 		return _mm512_mul_pd(converted, _mm512_set1_pd(0x1.0p-53));
-	}
-
-	template <cc::wide_prng_engine EngineT>
-	inline __m512i Random<EngineT>::range_512i_52(u64_t range) noexcept
-	{
-		__m512i source = EngineT::next_512i();
-		__m512i rangeVec = _mm512_set1_epi64(range);
-		__m512i low = _mm512_madd52lo_epu64(_mm512_setzero_si512(), source, rangeVec);
-		__mmask8 mask = _mm512_cmplt_epu64_mask(low, rangeVec);
-
-		if (mask) [[unlikely]]
-		{
-			__m512i t = _mm512_set1_epi64((1ULL << 52) % range);
-			while (mask = _mm512_cmplt_epu64_mask(low, t))
-			{
-				source = EngineT::next_512i();
-				low = _mm512_madd52lo_epu64(_mm512_setzero_si512(), source, rangeVec);
-			}
-		}
-
-		return _mm512_madd52hi_epu64(_mm512_setzero_si512(), source, rangeVec);
-	}
-
-	template <cc::wide_prng_engine EngineT>
-	inline __m512i Random<EngineT>::range_512i_52(__m512i range) noexcept
-	{
-		__m512i source = EngineT::next_512i();
-		__m512i low = _mm512_madd52lo_epu64(_mm512_setzero_si512(), source, range);
-		__mmask8 mask = _mm512_cmplt_epu64_mask(low, range);
-
-		if (mask) [[unlikely]]
-		{
-			u64_t rangeArray[8];
-			_mm512_storeu_epi64(rangeArray, range);
-
-			rangeArray[0] = (1ULL << 52) % rangeArray[0];
-			rangeArray[1] = (1ULL << 52) % rangeArray[1];
-			rangeArray[2] = (1ULL << 52) % rangeArray[2];
-			rangeArray[3] = (1ULL << 52) % rangeArray[3];
-			rangeArray[4] = (1ULL << 52) % rangeArray[4];
-			rangeArray[5] = (1ULL << 52) % rangeArray[5];
-			rangeArray[6] = (1ULL << 52) % rangeArray[6];
-			rangeArray[7] = (1ULL << 52) % rangeArray[7];
-
-			__m512i t = _mm512_loadu_epi64(rangeArray);
-			while (mask = _mm512_cmplt_epu64_mask(low, t))
-			{
-				source = EngineT::next_512i();
-				low = _mm512_madd52lo_epu64(_mm512_setzero_si512(), source, range);
-			}
-		}
-
-		return _mm512_madd52hi_epu64(_mm512_setzero_si512(), source, range);
 	}
 }
