33c367fcbe08a513b6310db3d0ae432fe5eaa0e6
Adrian Kulawik <a.kulawik8@gmail.com>
2025-05-30 19:37:27 +0200

    Add refresh() method to statistics

--- core/include/evoavx/evoavx-impl/statistics.hpp
+++ core/include/evoavx/evoavx-impl/statistics.hpp
@@ -18,6 +18,7 @@ namespace evo
 		u64_t m_stagnation = 0;
 
 		void start();
+		void refresh(Island<S>& island);
 		void update(Island<S>& island, f64_t meanEstimate);
 	};
 }
@@ -32,16 +33,29 @@ namespace evo
 	}
 
 	template <cc::static_settings S>
-	inline void Statistics<S>::update(Island<S>& island, f64_t meanEstimate)
+	inline void Statistics<S>::refresh(Island<S>& island)
 	{
-		u64_t count = alignment_floor<f64_t>(island.m_indivCount);
-		u64_t extra = island.m_indivCount - count;
-		f64_t* scores = island.m_scores.get();
-		f64_t* last = scores + count;
+		const u64_t count = alignment_floor<f64_t>(island.m_indivCount);
+		const u32_t extra = (1 << (island.m_indivCount - count)) - 1;
+		const f64_t* const scores = island.m_scores.get();
+		const f64_t* const last = scores + count;
 
-		__m512d mean = _mm512_set1_pd(meanEstimate);
+		__m512d sum0 = _mm512_setzero_pd();
+		for (const f64_t* ptr = scores; ptr != last; ptr += g_vectorGenes)
+			sum0 = _mm512_add_pd(sum0, _mm512_load_pd(ptr));
+
+		if (extra)
+		{
+			__mmask8 mask = _cvtu32_mask8(extra);
+			__m512d data = _mm512_maskz_load_pd(mask, last);
+			sum0 = _mm512_mask_add_pd(sum0, mask, sum0, data);
+		}
+
+		const f64_t meanEstimate = _mm512_reduce_add_pd(sum0) / island.m_indivCount;
+
 		__m512d sum1 = _mm512_setzero_pd();
 		__m512d sum2 = _mm512_setzero_pd();
+		__m512d mean = _mm512_set1_pd(meanEstimate);
 		__m512d min = _mm512_set1_pd(std::numeric_limits<f64_t>::max());
 		__m512d max = _mm512_set1_pd(std::numeric_limits<f64_t>::lowest());
 		__m512i minPos = _mm512_setzero_si512();
@@ -49,7 +63,7 @@ namespace evo
 		__m512i crrPos = _mm512_set_epi64(7, 6, 5, 4, 3, 2, 1, 0);
 		__m512i inc = _mm512_set1_epi64(g_vectorGenes);
 
-		for (f64_t* ptr = scores; ptr != last; ptr += g_vectorGenes)
+		for (const f64_t* ptr = scores; ptr != last; ptr += g_vectorGenes)
 		{
 			__m512d data = _mm512_load_pd(ptr);
 			__m512d diff = _mm512_sub_pd(data, mean);
@@ -67,7 +81,86 @@ namespace evo
 
 		if (extra)
 		{
-			__mmask8 mask = _cvtu32_mask8((1 << extra) - 1);
+			__mmask8 mask = _cvtu32_mask8(extra);
+			__m512d data = _mm512_maskz_load_pd(mask, last);
+			__m512d diff1 = _mm512_sub_pd(data, mean);
+			__m512d diff2 = _mm512_mul_pd(diff1, diff1);
+			__mmask8 minMask = _mm512_cmplt_pd_mask(data, min);
+			__mmask8 maxMask = _mm512_cmplt_pd_mask(max, data);
+
+			minMask = _kand_mask8(minMask, mask);
+			maxMask = _kand_mask8(maxMask, mask);
+
+			sum1 = _mm512_mask_add_pd(sum1, mask, sum1, diff1);
+			sum2 = _mm512_mask_add_pd(sum2, mask, sum2, diff2);
+			min = _mm512_mask_mov_pd(min, minMask, data);
+			max = _mm512_mask_mov_pd(max, maxMask, data);
+			minPos = _mm512_mask_mov_epi64(minPos, minMask, crrPos);
+			maxPos = _mm512_mask_mov_epi64(maxPos, maxMask, crrPos);
+		}
+
+		m_minimum = _mm512_reduce_min_pd(min);
+		m_maximum = _mm512_reduce_max_pd(max);
+
+		alignas(g_vectorBytes) u64_t minPosArray[g_vectorGenes];
+		alignas(g_vectorBytes) u64_t maxPosArray[g_vectorGenes];
+
+		_mm512_store_epi64(minPosArray, minPos);
+		_mm512_store_epi64(maxPosArray, maxPos);
+
+		__m512d hmin512 = _mm512_set1_pd(m_minimum);
+		__m512d hmax512 = _mm512_set1_pd(m_maximum);
+		u32_t minBits = _cvtmask8_u32(_mm512_cmpeq_pd_mask(min, hmin512));
+		u32_t maxBits = _cvtmask8_u32(_mm512_cmpeq_pd_mask(max, hmax512));
+
+		m_minimumPos = minPosArray[_tzcnt_u32(minBits)];
+		m_maximumPos = maxPosArray[_tzcnt_u32(maxBits)];
+
+		f64_t hsum1 = _mm512_reduce_add_pd(sum1);
+		f64_t hsum2 = _mm512_reduce_add_pd(sum2);
+		f64_t k = static_cast<f64_t>(island.m_indivCount);
+
+		m_mean = meanEstimate + hsum1 / k;
+		m_stdev = std::sqrt(hsum2 / k - (hsum1 * hsum1) / (k * k));
+	}
+
+	template <cc::static_settings S>
+	inline void Statistics<S>::update(Island<S>& island, f64_t meanEstimate)
+	{
+		const u64_t count = alignment_floor<f64_t>(island.m_indivCount);
+		const u32_t extra = (1 << (island.m_indivCount - count)) - 1;
+		const f64_t* const scores = island.m_scores.get();
+		const f64_t* const last = scores + count;
+
+		__m512d sum1 = _mm512_setzero_pd();
+		__m512d sum2 = _mm512_setzero_pd();
+		__m512d mean = _mm512_set1_pd(meanEstimate);
+		__m512d min = _mm512_set1_pd(std::numeric_limits<f64_t>::max());
+		__m512d max = _mm512_set1_pd(std::numeric_limits<f64_t>::lowest());
+		__m512i minPos = _mm512_setzero_si512();
+		__m512i maxPos = _mm512_setzero_si512();
+		__m512i crrPos = _mm512_set_epi64(7, 6, 5, 4, 3, 2, 1, 0);
+		__m512i inc = _mm512_set1_epi64(g_vectorGenes);
+
+		for (const f64_t* ptr = scores; ptr != last; ptr += g_vectorGenes)
+		{
+			__m512d data = _mm512_load_pd(ptr);
+			__m512d diff = _mm512_sub_pd(data, mean);
+			__mmask8 minMask = _mm512_cmplt_pd_mask(data, min);
+			__mmask8 maxMask = _mm512_cmplt_pd_mask(max, data);
+
+			sum1 = _mm512_add_pd(diff, sum1);
+			sum2 = _mm512_fmadd_pd(diff, diff, sum2);
+			min = _mm512_mask_mov_pd(min, minMask, data);
+			max = _mm512_mask_mov_pd(max, maxMask, data);
+			minPos = _mm512_mask_mov_epi64(minPos, minMask, crrPos);
+			maxPos = _mm512_mask_mov_epi64(maxPos, maxMask, crrPos);
+			crrPos = _mm512_add_epi64(crrPos, inc);
+		}
+
+		if (extra)
+		{
+			__mmask8 mask = _cvtu32_mask8(extra);
 			__m512d data = _mm512_maskz_load_pd(mask, last);
 			__m512d diff1 = _mm512_sub_pd(data, mean);
 			__m512d diff2 = _mm512_mul_pd(diff1, diff1);
--- tests/src/test-statistics.hpp
+++ tests/src/test-statistics.hpp
@@ -1,7 +1,90 @@
 #include "pch.hpp"
 #include <evoavx/evoavx.hpp>
 
-TEST_CASE("Statistics are calculated with good accuracy")
+TEST_CASE("Statistics are calculated with good accuracy on refresh()")
+{
+	evo::Island<evo::DefaultStaticSettings> island;
+	evo::Statistics<evo::DefaultStaticSettings>& stats = island.m_statistics;
+
+	SECTION("Tiny data set")
+	{
+		constexpr evo::f64_t margin = 1e-8;
+
+		island.m_indivCount = 3;
+		island.m_scores = evo::unique<evo::f64_t[]>(evo::allocate<evo::f64_t>(8));
+
+		island.m_scores[0] = std::numbers::pi;
+		island.m_scores[1] = std::numbers::phi;
+		island.m_scores[2] = -std::numbers::e;
+
+		stats.start();
+		stats.refresh(island);
+
+		REQUIRE_THAT(stats.m_mean, Catch::Matchers::WithinAbs(0.6804482713, margin));
+		REQUIRE_THAT(stats.m_stdev, Catch::Matchers::WithinAbs(2.4824493912, margin));
+		REQUIRE_THAT(stats.m_minimum, Catch::Matchers::WithinAbs(-std::numbers::e, margin));
+		REQUIRE_THAT(stats.m_maximum, Catch::Matchers::WithinAbs(std::numbers::pi, margin));
+
+		REQUIRE(stats.m_minimumPos == 2);
+		REQUIRE(stats.m_maximumPos == 0);
+		REQUIRE(stats.m_generation == 0);
+		REQUIRE(stats.m_stagnation == 0);
+	}
+
+	SECTION("Small data set")
+	{
+		constexpr evo::f64_t margin = 1e-8;
+
+		island.m_indivCount = 25;
+		island.m_scores = evo::unique<evo::f64_t[]>(evo::allocate<evo::f64_t>(32));
+
+		for (evo::i32_t i = 0; i < 5; i++)
+		{
+			evo::f64_t x = static_cast<evo::f64_t>(i);
+
+			island.m_scores[(evo::u64_t)i * 5 + 0] = std::numbers::phi * x;
+			island.m_scores[(evo::u64_t)i * 5 + 1] = std::numbers::pi * (x + 1.0);
+			island.m_scores[(evo::u64_t)i * 5 + 2] = -std::numbers::sqrt2 * x;
+			island.m_scores[(evo::u64_t)i * 5 + 3] = -std::numbers::inv_sqrtpi;
+			island.m_scores[(evo::u64_t)i * 5 + 4] = std::numbers::e * x * x;
+		}
+
+		stats.start();
+		stats.refresh(island);
+
+		REQUIRE_THAT(stats.m_mean, Catch::Matchers::WithinAbs(5.1155840401, margin));
+		REQUIRE_THAT(stats.m_stdev, Catch::Matchers::WithinAbs(10.2848561845, margin));
+		REQUIRE_THAT(stats.m_minimum, Catch::Matchers::WithinAbs(-5.6568542495, margin));
+		REQUIRE_THAT(stats.m_maximum, Catch::Matchers::WithinAbs(43.4925092553, margin));
+
+		REQUIRE(stats.m_minimumPos == 22);
+		REQUIRE(stats.m_maximumPos == 24);
+		REQUIRE(stats.m_generation == 0);
+		REQUIRE(stats.m_stagnation == 0);
+	}
+
+	SECTION("Large data set")
+	{
+		island.m_indivCount = 1ULL << 20;
+		island.m_scores = evo::unique<evo::f64_t[]>(evo::allocate<evo::f64_t>(evo::alignment_ceil<evo::f64_t>(island.m_indivCount)));
+
+		std::mt19937_64 mt(42);
+		std::uniform_real_distribution<double> uniform(-std::numbers::pi * 1e5, std::numbers::e * 1e6);
+
+		for (evo::u64_t i = 0; i < island.m_indivCount; i++)
+			island.m_scores[i] = uniform(mt);
+
+		stats.start();
+		stats.refresh(island);
+
+		REQUIRE_THAT(stats.m_mean, Catch::Matchers::WithinRel(1202061.282, 0.01));
+		REQUIRE_THAT(stats.m_stdev, Catch::Matchers::WithinRel(875390.3409, 0.01));
+		REQUIRE_THAT(stats.m_minimum, Catch::Matchers::WithinRel(-std::numbers::pi * 1e5, 0.01));
+		REQUIRE_THAT(stats.m_maximum, Catch::Matchers::WithinRel(std::numbers::e * 1e6, 0.01));
+	}
+}
+
+TEST_CASE("Statistics are calculated with good accuracy on update()")
 {
 	evo::Island<evo::DefaultStaticSettings> island;
 	evo::Statistics<evo::DefaultStaticSettings>& stats = island.m_statistics;
