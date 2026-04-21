ff869d8c2dde4c3c38f81712c7e99cb83303bfd8
Adrian Kulawik <a.kulawik8@gmail.com>
2026-04-21 15:33:46 +0200

    Add custom main() and update Catch2

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -4,3 +4,12 @@
 #include "test-random.hpp"
 #include "test-shuffle.hpp"
 #include "test-statistics.hpp"
+
+int main(int argc, char** argv)
+{
+	MPI_Init(&argc, &argv);
+	int result = Catch::Session().run(argc, argv);
+
+	MPI_Finalize();
+	return result;
+}
